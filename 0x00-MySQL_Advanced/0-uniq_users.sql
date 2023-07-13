4eb894c33a76be58f7f8263bcf297c958013d252
Meacha1 <meachattd@gmail.com>
2023-07-13 16:36:58 +0300

    task0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
--- Create a user table
--- with unique email addresses and names 
+-- Create a user table with unique email addresses
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
+    name VARCHAR(255)
 );
