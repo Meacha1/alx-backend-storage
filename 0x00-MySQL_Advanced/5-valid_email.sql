d6075c4f857ae7e1984b003773d6f9645a0a3aba
Meacha1 <meachattd@gmail.com>
2023-07-13 18:46:47 +0300

    Email validation

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,8 +4,9 @@ CREATE TRIGGER reset_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-  IF OLD.email <> NEW.email THEN
+  IF OLD.email != NEW.email THEN
     SET NEW.valid_email = 0;
   END IF;
 END$$
+
 DELIMITER ;
