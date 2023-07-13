1088438421659e8c3ff2a58feb86f207d0427d98
Meacha1 <meachattd@gmail.com>
2023-07-13 18:53:48 +0300

    Email validation

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,4 +1,5 @@
---SQL script that creates a trigger that resets the attribute valid_email only when the email has been changed
+--SQL script that creates a trigger that resets the
+--attribute valid_emailonly when the email has been changed
 DELIMITER $$
 CREATE TRIGGER reset 
 BEFORE UPDATE
