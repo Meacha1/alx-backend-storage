24812798cee07d946ba004c6005c6c44721d0518
Meacha1 <meachattd@gmail.com>
2023-07-18 10:47:20 -0700

    Top

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -9,15 +9,18 @@ def top_students(mongo_collection):
     Retrieves all students from the MongoDB collection and sorts them by average score.
     Returns a list of students with their average score included.
     """
-    students = mongo_collection.find()
-
-    # Calculate the average score for each student
-    for student in students:
-        total_score = sum(topic['score'] for topic in student['topics'])
-        average_score = total_score / len(student['topics'])
-        student['averageScore'] = average_score
-
-    # Sort the students by average score in descending order
-    sorted_students = sorted(students, key=lambda student: student['averageScore'], reverse=True)
-
-    return sorted_students
+    return mongo_collection.aggregate([
+        {
+            '$project': {
+                'name': '$name',
+                'averageScore': {
+                    '$avg': "$topics.score"
+                }
+            }
+        },
+        {
+            '$sort': {
+                "averageScore": -1
+            }
+        }
+    ])
