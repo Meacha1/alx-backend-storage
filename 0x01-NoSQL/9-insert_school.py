26a2d0f68f417fef04cc8f9d964685810e63b1c7
Meacha1 <meachattd@gmail.com>
2023-07-18 08:52:37 -0700

    insert

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module that provides a function to insert a new document in a collection based on kwargs.
+"""
+from pymongo.collection import Collection
+from typing import Any
+
+
+def insert_school(mongo_collection: Collection, **kwargs: Any) -> Any:
+    """
+    Inserts a new document into the given collection based on keyword arguments.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The collection object.
+        **kwargs: Keyword arguments representing the fields and values of the document.
+
+    Returns:
+        Any: The new _id of the inserted document.
+    """
+    document = kwargs
+    result = mongo_collection.insert_one(document)
+    new_id = result.inserted_id
+    return new_id
