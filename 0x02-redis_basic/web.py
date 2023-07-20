9508745deca865301a8c3e6f20bc9d3d79e59ad2
Meacha1 <meachattd@gmail.com>
2023-07-20 03:41:43 -0700

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,74 +2,50 @@
 '''
 Web Tools Module - Caching and Request Tracking Utilities
 '''
+
 import redis
 import requests
-import time
 from functools import wraps
 from typing import Callable
 
-# Create a Redis client
+
+# Redis instance for caching and tracking
 redis_client = redis.Redis()
 
-def caching_decorator(expiration_time: int):
-    """
-    Caching decorator to cache the function results with an expiration time.
 
-    Args:
-        expiration_time (int): The time in seconds for which the result should be cached.
+def cache_response(method: Callable) -> Callable:
+    '''
+    Caches the output of fetched data and tracks the
+    number of requests made to a URL.
+    '''
+    @wraps(method)
+    def wrapper(url) -> str:
+        '''
+        Wrapper function for caching the output and tracking the request.
+        '''
+        # Increment the number of requests made to the URL
+        redis_client.incr(f'count:{url}')
 
-    Returns:
-        Callable: The wrapped function that caches the results.
-    """
-    def decorator(func):
-        @wraps(func)
-        def wrapper(url: str) -> str:
-            """
-            Wrapper function for caching and tracking the number of accesses.
+        # Check if the result is already cached
+        result = redis_client.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
 
-            Args:
-                url (str): The URL to fetch the HTML content from.
+        # Fetch the data if not cached, and store it in Redis
+        # with an expiration time of 10 seconds
+        result = method(url)
+        redis_client.set(f'count:{url}', 0)
+        redis_client.setex(f'result:{url}', 10, result)
 
-            Returns:
-                str: The HTML content of the URL.
-            """
-            # Increment the number of accesses for the URL
-            redis_client.incr(f'count:{url}')
+        return result
 
-            # Check if the result is already cached
-            result = redis_client.get(f'result:{url}')
-            if result:
-                return result.decode('utf-8')
+    return wrapper
 
-            # Fetch the HTML content if not cached, and store it in Redis with an expiration time
-            result = func(url)
-            redis_client.setex(f'result:{url}', expiration_time, result)
 
-            return result
-        return wrapper
-    return decorator
-
-@caching_decorator(expiration_time=10)
+@cache_response
 def get_page(url: str) -> str:
-    """
-    Fetches the HTML content of a particular URL and returns it.
-
-    Args:
-        url (str): The URL to fetch the HTML content from.
-
-    Returns:
-        str: The HTML content of the URL.
-    """
-    response = requests.get(url)
-    return response.text
-
-# Example usage
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/https://www.example.com"
-    for i in range(5):
-        content = get_page(url)
-        print(f"Access {i + 1}: {content}")
-
-    access_count = redis_client.get(f"count:{url}")
-    print(f"Number of accesses for {url}: {int(access_count)}")
-
+    '''
+    Fetches the content of a URL and returns it, caching the
+    response and tracking the request count.
+    '''
+    return requests.get(url).text
