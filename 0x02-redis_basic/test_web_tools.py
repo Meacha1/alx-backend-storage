c803238e269958b730f0ffa27bf3df1933b03c1e
Meacha1 <meachattd@gmail.com>
2023-07-20 03:30:57 -0700

    web

--- 0x02-redis_basic/test_web_tools.py
+++ 0x02-redis_basic/test_web_tools.py
@@ -4,19 +4,13 @@ import time
 import web
 
 # Example URLs for testing
-url1 = "https://www.example.com"
-url2 = "https://www.google.com"
-url3 = "https://www.github.com"
+url1 = "http://slowwly.robertomurray.co.uk"
 
 def test_get_page():
     # Fetch the same URL multiple times to check caching behavior
     for i in range(5):
         content = web.get_page(url1)
         print(f"Access {i + 1}: {content}")
-
-    # Fetch different URLs to check separate caching
-    content2 = web.get_page(url2)
-    content3 = web.get_page(url3)
 
     # Wait for the cache to expire (10 seconds) and fetch the same URL again
     time.sleep(11)
@@ -25,12 +19,6 @@ def test_get_page():
 
     # Check the number of requests made to the URLs
     request_count_url1 = web.redis_client.get(f'count:{url1}')
-    request_count_url2 = web.redis_client.get(f'count:{url2}')
-    request_count_url3 = web.redis_client.get(f'count:{url3}')
-
-    print(f"Number of requests to {url1}: {int(request_count_url1)}")
-    print(f"Number of requests to {url2}: {int(request_count_url2)}")
-    print(f"Number of requests to {url3}: {int(request_count_url3)}")
 
 if __name__ == "__main__":
     test_get_page()
