99bcff072a7c1dbf2fd7037d6374cf348b829c3b
Meacha1 <meachattd@gmail.com>
2023-07-20 02:27:30 -0700

    6

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,13 +2,40 @@
 """
 web.py - Get Page with Caching
 """
-import requests
 import redis
+import requests
 import time
+from functools import wraps
 
+# Create a Redis client
+redis_client = redis.Redis()
+
+def caching_decorator(expiration_time: int):
+    """
+    Caching decorator to cache the function results with an expiration time.
+
+    Args:
+        expiration_time (int): The time in seconds for which the result should be cached.
+
+    Returns:
+        Callable: The wrapped function that caches the results.
+    """
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            key = f"cache:{func.__name__}:{args}"
+            result = redis_client.get(key)
+            if result is None:
+                result = func(*args, **kwargs)
+                redis_client.setex(key, expiration_time, result)
+            return result
+        return wrapper
+    return decorator
+
+@caching_decorator(expiration_time=10)
 def get_page(url: str) -> str:
     """
-    Fetches the HTML content of a URL and caches the result with an expiration time of 10 seconds.
+    Fetches the HTML content of a particular URL and returns it.
 
     Args:
         url (str): The URL to fetch the HTML content from.
@@ -16,25 +43,15 @@ def get_page(url: str) -> str:
     Returns:
         str: The HTML content of the URL.
     """
-    cache_key = f"count:{url}"
-
-    # Connect to Redis
-    redis_client = redis.Redis()
-
-    # Increment the access count for the URL
-    redis_client.incr(cache_key)
-
-    # Get the access count and set the expiration time to 10 seconds
-    access_count = int(redis_client.get(cache_key) or 0)
-    redis_client.expire(cache_key, 10)
-
-    # Fetch the HTML content of the URL using requests
     response = requests.get(url)
+    return response.text
 
-    return f"Access Count: {access_count}, Content: {response.text}"
-
-
+# Example usage
 if __name__ == "__main__":
-    # Test the get_page function with a sample URL
-    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://example.com"
-    print(get_page(url))
+    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/https://www.example.com"
+    for i in range(5):
+        content = get_page(url)
+        print(f"Access {i + 1}: {content}")
+
+    access_count = redis_client.get(f"count:{url}")
+    print(f"Number of accesses for {url}: {int(access_count)}")
